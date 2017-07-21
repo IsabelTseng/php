77575e30fda8db86676bea2843cf6ab6a763410e
IsabelTseng <Isabel870310@gmail.com>
2017-07-21 10:26:02 +0800

    init20170721

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="zh-Hant">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap Page</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+        <link href="css/jumbotron.css" rel="stylesheet">
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-heart" aria-hiden="true"></span> PHP project</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse text-right">
+                    <a href="#" class="btn btn-success">Sign In</a>
+                </div>
+            </div>
+        </nav>
+        <div class="jumbotron">
+            <div class="container">
+                <h1>OWO//</h1>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <table class="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>序號</th>
+                            <th>標題</th>
+                            <th>建立日期</th>
+                            <th>動作</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                    
+                </table>
+            </div>
+            <hr>
+            <footer>
+                <p class="text-center">&copy; Company 2017</p>
+            </footer>
+        </div>
+    </body>
+</html>
